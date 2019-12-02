b22e30bf9b6bfd39c0aede9210bb4a94d1f6f91e
dkawabata <21374877+dkawabata@users.noreply.github.com>
2019-12-02 23:22:41 +0900

    Todo一覧表示

--- src/app.js
+++ src/app.js
@@ -1,3 +1,25 @@
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import TODOS from './graphql/Todos.gql';
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'http://localhost:8080/v1/graphql'
+});
+
+const client = new ApolloClient({
+  cache,
+  link
+});
+
 export class App {
-  message = 'Hello World!';
+  heading = 'Todos';
+  todos = [];
+
+  constructor() {
+    client.query({
+      query: TODOS
+    }).then(data => this.todos = data.data.todos);
+  }
 }
