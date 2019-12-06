6b853c1e5065a61d6918127c2b0d55c23ba378cd
dkawabata <21374877+dkawabata@users.noreply.github.com>
2019-12-07 00:18:13 +0900

    新しいTodoを追加できるようにする

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 import TODOS from './graphql/Todos.gql';
+import INSERT_TODO from './graphql/InsertTodo.gql';
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
@@ -16,10 +17,29 @@ const client = new ApolloClient({
 export class App {
   heading = 'Todos';
   todos = [];
+  todoDescription = ''
 
   constructor() {
     client.query({
       query: TODOS
     }).then(data => this.todos = data.data.todos);
+  }
+
+  addTodo() {
+    if (this.todoDescription) {
+      client.mutate({
+        mutation: INSERT_TODO,
+        variables: { description: this.todoDescription },
+        update: (store, { data }) => {
+          let { todos } = store.readQuery({ query: TODOS });
+          todos.push(data.insert_todos.returning[0]);
+          store.writeQuery({
+            query: TODOS,
+            data: { 'Todos': todos }
+          });
+        }
+      });
+      this.todoDescription = '';
+    }
   }
 }
